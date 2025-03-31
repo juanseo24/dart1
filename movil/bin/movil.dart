98a53814a826bf8643278f1110de6e545d6376c8
juanseo24 <villegasvelez2007@gmail.com>
2025-03-31 14:24:45 -0500

    primer commit

---
+++ movil/bin/movil.dart
@@ -0,0 +1,5 @@
+import 'package:movil/movil.dart' as movil;
+
+void main(List<String> arguments) {
+  print('Hello world: ${movil.calculate()}!');
+}
---
+++ movil/test/movil_test.dart
@@ -0,0 +1,8 @@
+import 'package:movil/movil.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
